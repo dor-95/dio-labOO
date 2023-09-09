043d1d829b925b9f8332a4ccbf1516d3f0d06226
Diego <diego.rocha@edu.unipar.br>
2023-09-09 17:37:52 -0300

    feat: implemented methods: save and list orders

--- src/one/digitalinovation/laboojava/negocio/PedidoNegocio.java
+++ src/one/digitalinovation/laboojava/negocio/PedidoNegocio.java
@@ -57,15 +57,15 @@ public class PedidoNegocio {
      */
     public void salvar(Pedido novoPedido, Cupom cupom) {
 
-        //Definir padrão código
-        //Pegar data do dia corrente
-        //Formatar código
+        String codigo = "PE%4d%2d%04d";
+        LocalDate hoje = LocalDate.now();
+        codigo = String.format(codigo, hoje.getYear(), hoje.getMonthValue(), bancoDados.getPedidos().length);
 
-        //Setar código no pedido
-        //Setar cliente no pedido
-        //Calcular e set total
-        //Adicionar no banco
-        //Mensagem
+        novoPedido.setCodigo(codigo);
+        novoPedido.setCliente(bancoDados.getCliente());
+        novoPedido.setTotal(calcularTotal(novoPedido.getProdutos(), cupom));
+        bancoDados.adicionarPedido(novoPedido);
+        System.out.println("Pedido salvo com sucesso.");
     }
 
     /**
@@ -95,6 +95,17 @@ public class PedidoNegocio {
     /**
      * Lista todos os pedidos realizados.
      */
-    //TODO Método de listar todos os pedidos
+    public void listarTodosPedidos() {
+
+        if (bancoDados.getPedidos().length == 0) {
+            System.out.println("Não existem pedidos cadastrados");
+            return;
+        }
+
+        for (Pedido pedido : bancoDados.getPedidos()) {
+            System.out.println(pedido);
+        }
+
+    }
 
 }
