1358677ff0c9daa1d6c8e20581b9ef54adb09e95
Diego <diego.rocha@edu.unipar.br>
2023-09-09 16:47:07 -0300

    feat: created class Pedido

--- src/one/digitalinovation/laboojava/entidade/Pedido.java
+++ src/one/digitalinovation/laboojava/entidade/Pedido.java
@@ -8,11 +8,68 @@ import java.util.List;
  * @author thiago leite
  */
 public class Pedido {
+    private String codigo;
+    private Cliente cliente;
+    private List<Produto> produtos;
+    private double total;
 
-    //TODO Preencher esta classe
+    public Pedido() {
+        this.produtos = new ArrayList<>();
+    }
 
-    //codigo
-    //cliente
-    //produtos
-    //total
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    private String getProdutosComprados() {
+        StringBuilder produtos = new StringBuilder();
+        produtos.append("[");
+        for (Produto produto: getProdutos()) {
+            produtos.append(produto.toString());
+            produtos.append("Qtd:");
+            produtos.append(produto.getQuantidade());
+            produtos.append(" ");
+        }
+        produtos.append("]");
+
+        return produtos.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "codigo='" + codigo + '\'' +
+                ", cliente=" + cliente +
+                ", produtos=" + getProdutosComprados() +
+                ", total=" + total +
+                '}';
+    }
 }
