4963fbaa949f5007bd0c8cdaf9c0306b4669dcd2
Diego <diego.rocha@edu.unipar.br>
2023-09-09 20:08:50 -0300

    feat: created class Caderno and enum Materias

---
+++ src/one/digitalinovation/laboojava/entidade/Caderno.java
@@ -0,0 +1,31 @@
+package one.digitalinovation.laboojava.entidade;
+
+import one.digitalinovation.laboojava.entidade.constantes.Materias;
+
+public class Caderno extends Produto {
+
+    private Materias materias;
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public double calcularFrete() {
+        return 0;
+    }
+
+    public Materias getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(Materias materias) {
+        this.materias = materias;
+    }
+
+    @Override
+    public String toString() {
+        return "Caderno{" +
+                "materias=" + materias +
+                '}';
+    }
+}
